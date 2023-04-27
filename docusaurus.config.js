14e2897c1b11aee8db08c6e6e2e4f46115ae90da
Anthony Neumann <anthony.neumann@idfive.com>
2023-04-27 16:14:24 -0400

    Updated index reference

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -141,12 +141,12 @@ const config = {
       },
       algolia: {
         // The application ID provided by Algolia
-        appId: '8MYF53CSRP',
+        appId: '2PJMDLEUFN',
 
         // Public API key: it is safe to commit it
-        apiKey: '1c924a01066b640ce5223e7b52c2f102',
+        apiKey: '6beee8a4716c3590aacfed0f1df1fd67',
 
-        indexName: 'prod_idfivedocs',
+        indexName: 'developers-idfive',
 
         // Optional: see doc section below
         contextualSearch: true,
@@ -166,7 +166,114 @@ const config = {
         // Optional: path for search page that enabled by default (`false` to disable it)
         searchPagePath: 'search',
 
-        //... other Algolia params
+
+
+        // START UNKNOWN VALUES
+
+        rateLimit: 8,
+        maxDepth: 10,
+        startUrls: ['https://developers.idfive.com/'],
+        sitemaps: ['https://developers.idfive.com/sitemap.xml'],
+        ignoreCanonicalTo: true,
+        discoveryPatterns: ['https://developers.idfive.com/**'],
+        actions: [
+          {
+            indexName: 'prod_idfivedocs',
+            pathsToMatch: ['https://developers.idfive.com/**'],
+            recordExtractor: ({ $, helpers }) => {
+              // priority order: deepest active sub list header -> navbar active item -> 'Documentation'
+              const lvl0 =
+                $(
+                  '.menu__link.menu__link--sublist.menu__link--active, .navbar__item.navbar__link--active'
+                )
+                  .last()
+                  .text() || 'Documentation';
+
+              return helpers.docsearch({
+                recordProps: {
+                  lvl0: {
+                    selectors: '',
+                    defaultValue: lvl0,
+                  },
+                  lvl1: ['header h1', 'article h1'],
+                  lvl2: 'article h2',
+                  lvl3: 'article h3',
+                  lvl4: 'article h4',
+                  lvl5: 'article h5, article td:first-child',
+                  lvl6: 'article h6',
+                  content: 'article p, article li, article td:last-child',
+                },
+                indexHeadings: true,
+                aggregateContent: true,
+                recordVersion: 'v3',
+              });
+            },
+          },
+        ],
+        initialIndexSettings: {
+          YOUR_INDEX_NAME: {
+            attributesForFaceting: [
+              'type',
+              'lang',
+              'language',
+              'version',
+              'docusaurus_tag',
+            ],
+            attributesToRetrieve: [
+              'hierarchy',
+              'content',
+              'anchor',
+              'url',
+              'url_without_anchor',
+              'type',
+            ],
+            attributesToHighlight: ['hierarchy', 'content'],
+            attributesToSnippet: ['content:10'],
+            camelCaseAttributes: ['hierarchy', 'content'],
+            searchableAttributes: [
+              'unordered(hierarchy.lvl0)',
+              'unordered(hierarchy.lvl1)',
+              'unordered(hierarchy.lvl2)',
+              'unordered(hierarchy.lvl3)',
+              'unordered(hierarchy.lvl4)',
+              'unordered(hierarchy.lvl5)',
+              'unordered(hierarchy.lvl6)',
+              'content',
+            ],
+            distinct: true,
+            attributeForDistinct: 'url',
+            customRanking: [
+              'desc(weight.pageRank)',
+              'desc(weight.level)',
+              'asc(weight.position)',
+            ],
+            ranking: [
+              'words',
+              'filters',
+              'typo',
+              'attribute',
+              'proximity',
+              'exact',
+              'custom',
+            ],
+            highlightPreTag: '<span class="algolia-docsearch-suggestion--highlight">',
+            highlightPostTag: '</span>',
+            minWordSizefor1Typo: 3,
+            minWordSizefor2Typos: 7,
+            allowTyposOnNumericTokens: false,
+            minProximity: 1,
+            ignorePlurals: true,
+            advancedSyntax: true,
+            attributeCriteriaComputedByMinProximity: true,
+            removeWordsIfNoResults: 'allOptional',
+            separatorsToIndex: '_',
+          },
+        },
+
+
+
+        // END UNKNOWN VALUES
+
       },
     }),
 };
