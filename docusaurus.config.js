5213c9171b0281c0610e43440a84a956096d4271
Brad Walker <brad.walker@idfive.com>
2025-05-19 16:33:58 -0400

    New idfive social preview image added

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,54 +4,54 @@
 // const lightCodeTheme = require('prism-react-renderer/themes/github');
 // const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-const {themes} = require('prism-react-renderer');
+const { themes } = require("prism-react-renderer");
 const lightCodeTheme = themes.github;
 const darkCodeTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'idfive Developer Documentation',
-  tagline: 'Documentation for idfive Developers and Clients',
-  favicon: 'img/favicon.png',
+  title: "idfive Developer Documentation",
+  tagline: "Documentation for idfive Developers and Clients",
+  favicon: "img/favicon.png",
 
   // Set the production url of your site here
-  url: 'https://developers.idfive.com',
+  url: "https://developers.idfive.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'idfive', // Usually your GitHub org/user name.
-  projectName: 'Developer-Documentation', // Usually your repo name.
+  organizationName: "idfive", // Usually your GitHub org/user name.
+  projectName: "Developer-Documentation", // Usually your repo name.
   trailingSlash: false,
-  deploymentBranch: 'gh-pages',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  deploymentBranch: "gh-pages",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
-  plugins: ['docusaurus-plugin-sass'],
+  plugins: ["docusaurus-plugin-sass"],
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/idfive/Developer-Documentation/tree/main/',
+            "https://github.com/idfive/Developer-Documentation/tree/main/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.scss'),
+          customCss: require.resolve("./src/css/custom.scss"),
         },
       }),
     ],
@@ -61,19 +61,19 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/idfive-docusaurus-preview-image.png",
       navbar: {
-        title: '',
+        title: "",
         logo: {
-          alt: 'idfive Logo',
-          src: 'img/logo-idfive.svg',
+          alt: "idfive Logo",
+          src: "img/logo-idfive.svg",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Documentation',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Documentation",
           },
           // {
           //   to: '/blog',
@@ -88,7 +88,7 @@ const config = {
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         // links: [
         //   {
         //     title: 'Docs',
@@ -138,18 +138,18 @@ const config = {
       },
       algolia: {
         // The application ID provided by Algolia
-        appId: '2PJMDLEUFN',
+        appId: "2PJMDLEUFN",
 
         // Public API key: it is safe to commit it
-        apiKey: '6beee8a4716c3590aacfed0f1df1fd67',
+        apiKey: "6beee8a4716c3590aacfed0f1df1fd67",
 
-        indexName: 'developers-idfive',
+        indexName: "developers-idfive",
 
         // Optional: see doc section below
         contextualSearch: true,
 
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-        externalUrlRegex: 'external\\.com|domain\\.com',
+        externalUrlRegex: "external\\.com|domain\\.com",
 
         // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
         // replaceSearchResultPathname: {
@@ -161,48 +161,46 @@ const config = {
         searchParameters: {},
 
         // Optional: path for search page that enabled by default (`false` to disable it)
-        searchPagePath: 'search',
-
-
+        searchPagePath: "search",
 
         // START UNKNOWN VALUES
 
         rateLimit: 8,
         maxDepth: 10,
-        startUrls: ['https://developers.idfive.com/'],
-        sitemaps: ['https://developers.idfive.com/sitemap.xml'],
+        startUrls: ["https://developers.idfive.com/"],
+        sitemaps: ["https://developers.idfive.com/sitemap.xml"],
         ignoreCanonicalTo: true,
-        discoveryPatterns: ['https://developers.idfive.com/**'],
+        discoveryPatterns: ["https://developers.idfive.com/**"],
         actions: [
           {
-            indexName: 'prod_idfivedocs',
-            pathsToMatch: ['https://developers.idfive.com/**'],
+            indexName: "prod_idfivedocs",
+            pathsToMatch: ["https://developers.idfive.com/**"],
             recordExtractor: ({ $, helpers }) => {
               // priority order: deepest active sub list header -> navbar active item -> 'Documentation'
               const lvl0 =
                 $(
-                  '.menu__link.menu__link--sublist.menu__link--active, .navbar__item.navbar__link--active'
+                  ".menu__link.menu__link--sublist.menu__link--active, .navbar__item.navbar__link--active"
                 )
                   .last()
-                  .text() || 'Documentation';
+                  .text() || "Documentation";
 
               return helpers.docsearch({
                 recordProps: {
                   lvl0: {
-                    selectors: '',
+                    selectors: "",
                     defaultValue: lvl0,
                   },
-                  lvl1: ['header h1', 'article h1'],
-                  lvl2: 'article h2',
-                  lvl3: 'article h3',
-                  lvl4: 'article h4',
-                  lvl5: 'article h5, article td:first-child',
-                  lvl6: 'article h6',
-                  content: 'article p, article li, article td:last-child',
+                  lvl1: ["header h1", "article h1"],
+                  lvl2: "article h2",
+                  lvl3: "article h3",
+                  lvl4: "article h4",
+                  lvl5: "article h5, article td:first-child",
+                  lvl6: "article h6",
+                  content: "article p, article li, article td:last-child",
                 },
                 indexHeadings: true,
                 aggregateContent: true,
-                recordVersion: 'v3',
+                recordVersion: "v3",
               });
             },
           },
@@ -210,51 +208,52 @@ const config = {
         initialIndexSettings: {
           YOUR_INDEX_NAME: {
             attributesForFaceting: [
-              'type',
-              'lang',
-              'language',
-              'version',
-              'docusaurus_tag',
+              "type",
+              "lang",
+              "language",
+              "version",
+              "docusaurus_tag",
             ],
             attributesToRetrieve: [
-              'hierarchy',
-              'content',
-              'anchor',
-              'url',
-              'url_without_anchor',
-              'type',
+              "hierarchy",
+              "content",
+              "anchor",
+              "url",
+              "url_without_anchor",
+              "type",
             ],
-            attributesToHighlight: ['hierarchy', 'content'],
-            attributesToSnippet: ['content:10'],
-            camelCaseAttributes: ['hierarchy', 'content'],
+            attributesToHighlight: ["hierarchy", "content"],
+            attributesToSnippet: ["content:10"],
+            camelCaseAttributes: ["hierarchy", "content"],
             searchableAttributes: [
-              'unordered(hierarchy.lvl0)',
-              'unordered(hierarchy.lvl1)',
-              'unordered(hierarchy.lvl2)',
-              'unordered(hierarchy.lvl3)',
-              'unordered(hierarchy.lvl4)',
-              'unordered(hierarchy.lvl5)',
-              'unordered(hierarchy.lvl6)',
-              'content',
+              "unordered(hierarchy.lvl0)",
+              "unordered(hierarchy.lvl1)",
+              "unordered(hierarchy.lvl2)",
+              "unordered(hierarchy.lvl3)",
+              "unordered(hierarchy.lvl4)",
+              "unordered(hierarchy.lvl5)",
+              "unordered(hierarchy.lvl6)",
+              "content",
             ],
             distinct: true,
-            attributeForDistinct: 'url',
+            attributeForDistinct: "url",
             customRanking: [
-              'desc(weight.pageRank)',
-              'desc(weight.level)',
-              'asc(weight.position)',
+              "desc(weight.pageRank)",
+              "desc(weight.level)",
+              "asc(weight.position)",
             ],
             ranking: [
-              'words',
-              'filters',
-              'typo',
-              'attribute',
-              'proximity',
-              'exact',
-              'custom',
+              "words",
+              "filters",
+              "typo",
+              "attribute",
+              "proximity",
+              "exact",
+              "custom",
             ],
-            highlightPreTag: '<span class="algolia-docsearch-suggestion--highlight">',
-            highlightPostTag: '</span>',
+            highlightPreTag:
+              '<span class="algolia-docsearch-suggestion--highlight">',
+            highlightPostTag: "</span>",
             minWordSizefor1Typo: 3,
             minWordSizefor2Typos: 7,
             allowTyposOnNumericTokens: false,
@@ -262,15 +261,12 @@ const config = {
             ignorePlurals: true,
             advancedSyntax: true,
             attributeCriteriaComputedByMinProximity: true,
-            removeWordsIfNoResults: 'allOptional',
-            separatorsToIndex: '_',
+            removeWordsIfNoResults: "allOptional",
+            separatorsToIndex: "_",
           },
         },
 
-
-
         // END UNKNOWN VALUES
-
       },
     }),
 };
